3d9d1a29e05466f179006b20f94c6ed57ca15e99
eternity-ux <1473608573@qq.com>
2024-06-28 10:36:06 +0800

    feat: 修改配置文件

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.etux.top": dockerHub,
+  "quay.etux.top": "https://quay.io",
+  "gcr.etux.top": "https://gcr.io",
+  "k8s-gcr.etux.top": "https://k8s.gcr.io",
+  "k8s.etux.top": "https://registry.k8s.io",
+  "ghcr.etux.top": "https://ghcr.io",
+  "cloudsmith.etux.top": "https://docker.cloudsmith.io",
+  "ecr.etux.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.etux.top": dockerHub,
 };
 
 function routeByHosts(host) {
